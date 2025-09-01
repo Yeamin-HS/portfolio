4b1aa7f17bc6b104e6bdf2ee106a348c2a99a148
Yeamin-HS <yeamin.hossain.shihab@g.bracu.ac.bd>
2025-09-01 13:09:31 +0600

    rebuild

--- Portfolio/tailwind.config.cjs
+++ Portfolio/tailwind.config.cjs
@@ -1,20 +1,10 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 module.exports = withMT({
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
